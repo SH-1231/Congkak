c741f06e0a8989444c9987796dbc90c807d87e3e
shaun <sefh2@cam.ac.uk>
2023-12-12 11:33:08 +0800

    adding checks for endgame

---
+++ src/congkak/moves/containers.py
@@ -0,0 +1,14 @@
+import dataclasses
+
+from congkak.board.constants import PITS_PER_SIDE
+from congkak.board.containers import Player
+@dataclasses.dataclass(frozen=True)
+class PlayerMove:
+    player_number: Player
+    pit_number: int
+
+    def __post_init__(self):
+        if self.pit_number> PITS_PER_SIDE:
+            raise ValueError(
+                f"Pit selected ({self.pit_number}) is not within max number of pits: {PITS_PER_SIDE}"
+            )
