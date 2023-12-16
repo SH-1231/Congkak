c66115c5de8c95cee769a9f3d9b595e60e1540a9
shaun <sefh2@cam.ac.uk>
2023-12-16 10:45:09 +0800

    adding display for endgame

--- tests/game/test_display.py
+++ tests/game/test_display.py
@@ -2,8 +2,14 @@ from importlib import metadata
 
 import numpy as np
 import pytest
-from pycongkak.board.containers import BoardState, Player, PlayerNumber
-from pycongkak.game.display import int_to_string_with_min_spacing, terminal_show_board
+from pycongkak.board.containers import BoardState, GameStatistics, Player, PlayerNumber
+from pycongkak.game.display import (
+    int_to_string_with_min_spacing,
+    terminal_show_board,
+    terminal_show_victory,
+)
+
+pycongkak_string = f"Pypycongkak@{metadata.version('pycongkak')} by Shaun Ho\n"
 
 
 def test_print_with_min_spacing() -> None:
@@ -25,38 +31,37 @@ def test_terminal_show_board_player_one(capsys: pytest.CaptureFixture) -> None:
             n_turns=3,
         )
     )
-    pycongkak_string_____ = f"Pypycongkak@{metadata.version('pycongkak')} by Shaun Ho\n"
     spacing = "\n"
-    turn_string________ = "Player 1's go, 3 turns remaining\n"
+    turn_string = "Player 1's go, 3 turns remaining\n"
     spacing = "\n"
     opponent_pit_number = "pit number:  1  2  3  4  5  6  7  \n"
     spacing = "\n"
-    opponent_number____ = "                   Player 2\n"
-    opponent_side______ = "         /-[ 1, 2, 3, 4, 5, 6, 7]-\\  [05]\n"
-    dividing_line______ = "        <-------------------------->\n"
-    player_side________ = "   [10]  \\-[ 7, 6, 5, 4, 3, 2, 1]-/ \n"
-    player_number______ = "                   Player 1\n"
+    opponent_number = "                   Player 2\n"
+    opponent_side = "         /-[ 1, 2, 3, 4, 5, 6, 7]-\\  [05]\n"
+    dividing_line = "        <-------------------------->\n"
+    player_side = "   [10]  \\-[ 7, 6, 5, 4, 3, 2, 1]-/ \n"
+    player_number = "                   Player 1\n"
     spacing = "\n"
-    player_pit_number__ = "pit number:  7  6  5  4  3  2  1  \n"
+    player_pit_number = "pit number:  7  6  5  4  3  2  1  \n"
     spacing = "\n"
-    selector_text______ = "Please select a pit number [0-6]: \n"
+    selector_text = "Please select a pit number [0-6]: \n"
 
     expected_string = (
-        pycongkak_string_____
+        pycongkak_string
         + spacing
-        + turn_string________
+        + turn_string
         + spacing
         + opponent_pit_number
         + spacing
-        + opponent_number____
-        + opponent_side______
-        + dividing_line______
-        + player_side________
-        + player_number______
+        + opponent_number
+        + opponent_side
+        + dividing_line
+        + player_side
+        + player_number
         + spacing
-        + player_pit_number__
+        + player_pit_number
         + spacing
-        + selector_text______
+        + selector_text
     )
     capture_result = capsys.readouterr()
     expected_print_statement = capture_result.out
@@ -78,38 +83,38 @@ def test_terminal_show_board_player_two(capsys: pytest.CaptureFixture) -> None:
         ),
         fixed_board=True,
     )
-    pycongkak_string_____ = f"Pypycongkak@{metadata.version('pycongkak')} by Shaun Ho\n"
+    pycongkak_string = f"Pypycongkak@{metadata.version('pycongkak')} by Shaun Ho\n"
     spacing = "\n"
-    turn_string________ = "Player 2's go, 3 turns remaining\n"
+    turn_string = "Player 2's go, 3 turns remaining\n"
     spacing = "\n"
     opponent_pit_number = "pit number:  1  2  3  4  5  6  7  \n"
     spacing = "\n"
-    opponent_number____ = "                   Player 2\n"
-    opponent_side______ = "         /-[ 1, 2, 3, 4, 5, 6, 7]-\\  [05]\n"
-    dividing_line______ = "        <-------------------------->\n"
-    player_side________ = "   [10]  \\-[ 7, 6, 5, 4, 3, 2, 1]-/ \n"
-    player_number______ = "                   Player 1\n"
+    opponent_number = "                   Player 2\n"
+    opponent_side = "         /-[ 1, 2, 3, 4, 5, 6, 7]-\\  [05]\n"
+    dividing_line = "        <-------------------------->\n"
+    player_side = "   [10]  \\-[ 7, 6, 5, 4, 3, 2, 1]-/ \n"
+    player_number = "                   Player 1\n"
     spacing = "\n"
-    player_pit_number__ = "pit number:  7  6  5  4  3  2  1  \n"
+    player_pit_number = "pit number:  7  6  5  4  3  2  1  \n"
     spacing = "\n"
-    selector_text______ = "Please select a pit number [0-6]: \n"
+    selector_text = "Please select a pit number [0-6]: \n"
 
     expected_string = (
-        pycongkak_string_____
+        pycongkak_string
         + spacing
-        + turn_string________
+        + turn_string
         + spacing
         + opponent_pit_number
         + spacing
-        + opponent_number____
-        + opponent_side______
-        + dividing_line______
-        + player_side________
-        + player_number______
+        + opponent_number
+        + opponent_side
+        + dividing_line
+        + player_side
+        + player_number
         + spacing
-        + player_pit_number__
+        + player_pit_number
         + spacing
-        + selector_text______
+        + selector_text
     )
     capture_result = capsys.readouterr()
     expected_print_statement = capture_result.out
@@ -129,39 +134,88 @@ def test_terminal_show_board_player_two(capsys: pytest.CaptureFixture) -> None:
         ),
         fixed_board=False,
     )
-    pycongkak_string_____ = f"Pypycongkak@{metadata.version('pycongkak')} by Shaun Ho\n"
+    pycongkak_string = f"Pypycongkak@{metadata.version('pycongkak')} by Shaun Ho\n"
     spacing = "\n"
-    turn_string________ = "Player 2's go, 3 turns remaining\n"
+    turn_string = "Player 2's go, 3 turns remaining\n"
     spacing = "\n"
     opponent_pit_number = "pit number:  1  2  3  4  5  6  7  \n"
     spacing = "\n"
-    opponent_number____ = "                   Player 1\n"
-    opponent_side______ = "         /-[ 1, 2, 3, 4, 5, 6, 7]-\\  [10]\n"
-    dividing_line______ = "        <-------------------------->\n"
-    player_side________ = "   [05]  \\-[ 7, 6, 5, 4, 3, 2, 1]-/ \n"
-    player_number______ = "                   Player 2\n"
+    opponent_number = "                   Player 1\n"
+    opponent_side = "         /-[ 1, 2, 3, 4, 5, 6, 7]-\\  [10]\n"
+    dividing_line = "        <-------------------------->\n"
+    player_side = "   [05]  \\-[ 7, 6, 5, 4, 3, 2, 1]-/ \n"
+    player_number = "                   Player 2\n"
     spacing = "\n"
-    player_pit_number__ = "pit number:  7  6  5  4  3  2  1  \n"
+    player_pit_number = "pit number:  7  6  5  4  3  2  1  \n"
     spacing = "\n"
-    selector_text______ = "Please select a pit number [0-6]: \n"
+    selector_text = "Please select a pit number [0-6]: \n"
 
     expected_string = (
-        pycongkak_string_____
+        pycongkak_string
         + spacing
-        + turn_string________
+        + turn_string
         + spacing
         + opponent_pit_number
         + spacing
-        + opponent_number____
-        + opponent_side______
-        + dividing_line______
-        + player_side________
-        + player_number______
+        + opponent_number
+        + opponent_side
+        + dividing_line
+        + player_side
+        + player_number
         + spacing
-        + player_pit_number__
+        + player_pit_number
         + spacing
-        + selector_text______
+        + selector_text
     )
     capture_result = capsys.readouterr()
     expected_print_statement = capture_result.out
+    assert expected_print_statement == expected_string
+
+
+def test_terminal_show_victory(capsys: pytest.CaptureFixture) -> None:
+    game_stats = GameStatistics(
+        winner=Player(
+            number=PlayerNumber.ONE, score=10, side=np.array([0, 0, 0, 0, 0, 0, 0])
+        ),
+        player_one=Player(
+            number=PlayerNumber.ONE, score=10, side=np.array([0, 0, 0, 0, 0, 0, 0])
+        ),
+        player_two=Player(
+            number=PlayerNumber.TWO, score=1, side=np.array([0, 0, 0, 0, 0, 0, 0])
+        ),
+        margin=10,
+    )
+
+    terminal_show_victory(game_stats)
+    capture_result = capsys.readouterr()
+    expected_print_statement = capture_result.out
+    game_ended_string = "<-- Game Ended -->\n"
+    winner_string_1 = "Winner: Player 1"
+    winner_string_2 = ", scoring 10\n"
+
+    expected_string = (
+        pycongkak_string + game_ended_string + winner_string_1 + winner_string_2
+    )
+    assert expected_print_statement == expected_string
+
+
+def test_terminal_show_draw(capsys: pytest.CaptureFixture) -> None:
+    game_stats = GameStatistics(
+        winner=None,
+        player_one=Player(
+            number=PlayerNumber.ONE, score=10, side=np.array([0, 0, 0, 0, 0, 0, 0])
+        ),
+        player_two=Player(
+            number=PlayerNumber.TWO, score=10, side=np.array([0, 0, 0, 0, 0, 0, 0])
+        ),
+        margin=10,
+    )
+
+    terminal_show_victory(game_stats)
+    capture_result = capsys.readouterr()
+    expected_print_statement = capture_result.out
+    game_ended_string = "<-- Game Ended -->\n"
+    draw_string = "Draw: Both players scored 10 marbles\n"
+
+    expected_string = pycongkak_string + game_ended_string + draw_string
     assert expected_print_statement == expected_string
